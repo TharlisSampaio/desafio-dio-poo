f8634f723ffb302828797a71ad20ffc458ff9a49
Tharlis Sampaio <tharlis.sampaio@outlook.com>
2024-09-04 09:32:28 -0500

    feat: Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String [] args){
+        Curso curso = new Curso();
+
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("descrição");
+        curso.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("descrição");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria Java");
+        mentoria.setDate(LocalDate.now());
+
+        System.out.println(mentoria);
     }
 }
